0779091e85569f55d0405f64bf7b5b179d7edd48
Researchnix <forschnix@gmail.com>
2020-05-02 21:18:51 -0400

    update

---
+++ may/jewelsAndStones.py
@@ -0,0 +1,18 @@
+# jewels and Stones
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        js = set(J)
+        j = 0
+        for s in S:
+            if s in J:
+                j += 1
+        return j
+
+
+J = "aAb"
+S = "aAAbbbb"
+
+s = Solution()
+print(s.numJewelsInStones(J,S))
+
